51681a7f7f20eb689c8a633d0003f3f34175a893
root <root@master.pilsen.pug>
2016-02-29 16:21:56 +0000

    Initial commit

---
+++ manifests/init.pp
@@ -0,0 +1,37 @@
+# Class: welcome
+# ===========================
+#
+class welcome (
+    $port = 8888,
+  ) {
+
+  package { 'nginx':
+    ensure => present,
+  }
+
+  file { ['/var', '/var/pug']:
+    ensure => directory
+  }
+
+  file { '/var/pug/index.html':
+    ensure => file,
+    content => '<html><body>PUG is awesome!</body></html>',
+    notify => Service['nginx'],
+  }
+
+  file { '/etc/nginx/default.conf':
+    ensure => file,
+    content => template('nginx/default.conf.erb'),
+    require => Package['nginx],
+    notify => Service['nginx'],
+  }
+
+  service { 'nginx':
+    ensure => running,
+    enable => true,
+    require => [
+      Package['nginx'],
+      File['/var/pug/index.html','/etc/nginx/default.conf'],
+    ]
+  }
+}
